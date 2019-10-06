6becf40265a8dbd7710dfb440cd96d298b27700e
Troy <troyjmcl85@gmail.com>
2019-10-05 20:45:44 -0600

    First iteration of seeding script for product collection

--- seed.js
+++ seed.js
@@ -12,12 +12,15 @@ var description = new Schema({
   item_Spec: {
     condition: String,
     brand: String,
-    year: Number,
     type: String,
-    size: Number,
     packaging: String,
-    rec_age: String,
     material: String,
+    rec_age: String,
+    char_family: String,
+    manfactured: String,
+    era: String,
+    year: Number,
+    size: Number,
     upc: Number
   },
   seller_msg: {
@@ -47,4 +50,78 @@ var ship_pay = new Schema ({
     type: String,
     pay_shipping: String
   }
-})
+});
+
+var Product = mongoose.model('Product', description);
+var Purchase = mongoose.model('Purchase', ship_pay)
+
+let seed = () => {
+  let clear = dropdb()
+  let random = randomProduct()
+  let data = []
+  for(let i =0; i < 10; i++) {
+    let currProd = {
+      item_number: i,
+      list_date: faker.date.recent(),
+      item_Spec: {
+        condition: faker.lorem.word(),
+        brand: random[0],
+        type: random[1],
+        packaging: random[2],
+        material: random[3],
+        rec_age: random[4],
+        char_family: random[5],
+        manfactured: random[6],
+        era: random[7],
+        year: random[8],
+        size: random[9],
+        upc: random[10]
+      },
+      seller_msg: {
+        prod_des: faker.lorem.paragraphs(),
+        item_des: faker.lorem.sentence(),
+        img_url: faker.image.image()
+      }
+    }
+    data.push(currProd)
+  }
+  Product.collection.insertMany(data,(err,docs) => {
+    if (err) {
+      return console.log(err)
+    } else {
+      console.log('Multiple documents added to collection')
+    }
+  })
+
+}
+
+let dropdb = () => {
+  Product.collection.drop()
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+    Purchase.collection.drop()
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+
+let randomProduct = () => {
+  let array = [];
+  for (let i = 0; i <= 10; i++) {
+    let exist = Math.floor(Math.random() * Math.floor(2))
+    if (exist) {
+      if (i <= 7 ) {
+        let str = faker.lorem.words();
+        array.push(str)
+      } else {
+        let val = faker.random.number();
+        array.push(val)
+      }
+    } else {
+      array.push('')
+    }
+  }
+  return array
+}
+
+seed()
