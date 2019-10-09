c4c769a318bb16f61ed9f14569c836a86e5cc0ba
Troy <troyjmcl85@gmail.com>
2019-10-09 15:39:54 -0600

    Client side component is making GET request- data returned is being added to props of component

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,25 @@
+import $ from 'jquery';
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data:[]
+    }
+  }
+
+  componentDidMount() {
+    $.get({
+      url: 'http://localhost:3005/description/1',
+      success: (res) => {
+        console.log(res)
+        this.setState({
+          data:res
+        })
+      },
+      dataType: 'JSON'
+    })
   }
 
   render() {
