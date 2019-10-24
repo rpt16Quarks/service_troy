5e583a5bac70acbc4d18900d7d1a98481e97f58f
Troy <troyjmcl85@gmail.com>
2019-10-23 20:09:25 -0600

    Finished styling of shipping and handling tab

--- client/components/ship_pay.jsx
+++ client/components/ship_pay.jsx
@@ -43,13 +43,28 @@ const ShipPay = (props) => {
       <ReturnPolicy returns={props.payment}/>
       <Paypal>
         <Header>Payment details</Header>
-        <p>Payment methods</p>
-        <img src="https://p.ebaystatic.com/aw/logos/logoPaypalCreditv2_157x55.png"></img>
-        <h2>Special financing available</h2>
-        <p>Select PayPal Credit at checkout to have the option to pay over time.</p>
-        <p>Qualifying purchases could enjoy No Interest if paid in full in 6 months on purchases of $99 or more. Other offers may also be available.</p>
-        <p>Interest will be charged to your account from the purchase date if the balance is not paid in full within 6 months. Minimum monthly payments are required. Subject to credit approval. See terms- opens in a new window or tab</p>
-        <p>The PayPal Credit account is issued by Synchrony Bank.</p>
+        <InfoTables>
+          <tbody>
+            <tr>
+              <td> Payment methods</td>
+            </tr>
+          </tbody>
+         </InfoTables>
+        <Credit>
+        <a href="http://www.credit-card-logos.com/"><img alt="Credit Card Logos" title="Credit Card Logos" src="http://www.credit-card-logos.com/images/multiple_credit-card-logos-1/credit_card_logos_17.gif" width="235" height="35" border="0" /></a>
+        </Credit>
+        <Offers>
+          <img src="https://p.ebaystatic.com/aw/logos/logoPaypalCreditv2_157x55.png"></img>
+          <OfferTxt>
+            <FinancingHeader>Special financing available</FinancingHeader>
+            <p>Select PayPal Credit at checkout to have the option to pay over time.</p>
+            <p>Qualifying purchases could enjoy No Interest if paid in full in 6 months on purchases of $99 or more. Other offers may also be available.</p>
+            <p>Interest will be charged to your account from the purchase date if the balance is not paid in full within 6 months. Minimum monthly payments are required. Subject to credit approval. <Link href="#">See terms</Link></p>
+            <p>The PayPal Credit account is issued by Synchrony Bank.</p>
+          </OfferTxt>
+        </Offers>
+        <Header>Seller's payment instructions</Header>
+        <p>See item description for checkout instructions.</p>
       </Paypal>
     </ShipContainer>
   )
@@ -80,6 +95,7 @@ const InfoTables = styled.table`
   table-layout: auto;
   width:100%;
   td {
+    line-height: 1.5em;
     border-bottom: 1px solid black;
     padding: 5px 10px 5px 15px;
   }
@@ -93,4 +109,26 @@ const DeliveryInfo = styled.p`
 const Link = styled.a`
   text-decoration:none;
   font-color: blue;
+`
+
+const Credit = styled.div`
+  border-bottom: 1px solid black;
+  padding 25px;
+  margin-bottom: 15px;
+`
+const Financing = styled.h2`
+  font-size: 16px;
+  font-color:blue;
+`
+const Offers = styled.div`
+  display:flex;
+  justify-content: space-around;
+  align-items: flex-start;
+`
+const OfferTxt = styled.div`
+  width: 70%
+  margin-right: 50px;
+`
+const FinancingHeader = styled(Header)`
+  color: blue;
 `
