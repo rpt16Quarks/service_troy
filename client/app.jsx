f65c90d825d7f13a1392dcfba362d02fd7bce05d
Troy <troyjmcl85@gmail.com>
2019-11-07 20:11:50 -0700

    Last few styling adjustments, button styling now tied to state.page and not item focus, added box shadow to tab buttons, changed blue font color

--- client/app.jsx
+++ client/app.jsx
@@ -50,8 +50,8 @@ class App extends React.Component {
     return (
       <div>
         <Menu>
-          <Button autoFocus id="desc" onClick={this.handleDescClick}>Description</Button>
-          <Button id="shippay" onClick={this.handleSPClick}>Shipping and payments</Button>
+          <Button id="desc" onClick={this.handleDescClick} tab={this.state.page}>Description</Button>
+          <SPButton id="shippay" onClick={this.handleSPClick} tab={this.state.page}>Shipping and payments</SPButton>
           <Report>
             <p>Report item</p>
           </Report>
@@ -73,7 +73,7 @@ const Menu = styled.div`
   align-items:flex-end;
 `
 const Report = styled.div`
-  border-bottom: 1px solid black;
+  border-bottom: 1px solid #cccccc;
   flex-grow:3
   font-size: 12px;
   text-align: right;
@@ -84,24 +84,28 @@ const Report = styled.div`
 `
 
 const Button = styled.button`
-  background: #f2f2f2;
+  background: ${(props) => props.tab === 0 ? "white" : "#f2f2f2" };
   border-top-left-radius: 3px;
   border-top-right-radius: 3px;
-  border: 1px solid black;
-  color: blue;
+  border: 1px solid #cccccc;
+  color: #0654ba;
   font-size: 14px;
   padding: 5px 10px 5px 10px;
   cursor: pointer;
-
-  &:focus {
-    outline:0;
-    background: white;
-    border-bottom: 1px solid white;
-  }
+  outline:0;
+  box-shadow: ${(props) => props.tab === 0 ? "4px -1.5px #e6e3e3" : " 0px 0px #cccccc"}
+  border-bottom: ${(props) => props.tab === 0 ? "1px solid white" : "1px solid #cccccc"}
+  z-index: 1
+`
+const SPButton = styled(Button)`
+  background: ${(props) => props.tab === 1 ? "white" : "#f2f2f2" };
+  border-bottom: ${(props) => props.tab === 1 ? "1px solid white" : "1px solid #cccccc" }
+  box-shadow: ${(props) => props.tab === 1 ? "4px -1.5px #e6e3e3" : " 0px 0px #cccccc"}
+  z-index: 0
 `
 
 const Content = styled.div`
-  border: 1px solid black;
+  border: 1px solid #cccccc;
   border-top: 1px solid white;
   margin: 0px 5px 10px 5px;
   padding: 0px 25px 25px 25px;
